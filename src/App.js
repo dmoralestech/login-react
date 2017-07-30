5424f56d1ab09440d93c114f92bd73b6788d6849
darwinmorales <darwin.morales.tech@gmail.com>
2017-07-30 15:59:56 +1000

    add material-ui  - fix bug

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import {Card, CardTitle} from 'material-ui/Card';
+import {Card, CardTitle, MuiThemeProvider} from 'material-ui';
 import './App.css';
 
-const App = () => (
-    <Card className="container">
-        <CardTitle title="React Application" subtitle="This is the home page." />
-    </Card>
-);
+const App = () => {
+    return (
+        <MuiThemeProvider>
+            <Card className="container">
+                <CardTitle title="React Application" subtitle="This is the home page." />
+            </Card>
+        </MuiThemeProvider>
+    );
+}
 
 export default App;
